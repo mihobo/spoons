761fa117ccb678f65b81e53c018ee0f25e9865f5
Simon Ashbery <simonashbery@simons-mbp.guest.visitor>
2019-01-26 15:23:57 +0000

    adds rotation controll to SPOOOOOOOOON

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,34 +5,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   private Rigidbody rb;
+  private Transform transform;
   public float speed = 5;
   public float rotationSpeed = 5;
   // Start is called before the first frame update
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    transform = GetComponent<Transform>();
   }
 
   // Update is called once per frame
   void Update()
   {
+    float verticalRotation = Input.GetAxis("LookVertical");
+    float horizontalRotation = Input.GetAxis("LookHorizontal");
 
+    Vector3 rotationVector = new Vector3(verticalRotation, horizontalRotation, 0);
+
+    transform.Rotate(rotationVector);
   }
 
   void FixedUpdate()
   {
     float forwardMovement = Input.GetAxis("Vertical");
     float strafeMovement = Input.GetAxis("Horizontal");
-    float verticalRotation = Input.GetAxis("LookVertical");
-    float horizontalRotation = Input.GetAxis("LookHorizontal");
-    Vector3 rotationVector = new Vector3(verticalRotation, horizontalRotation, 0);
 
-    Debug.Log(verticalRotation);
-    Debug.Log(horizontalRotation);
+    Debug.Log(rb.velocity);
+    if (rb.velocity.x >= 10)
+    {
+      rb.velocity = new Vector3(10, rb.velocity.y, rb.velocity.z);
+    }
+    if (rb.velocity.z >= 10)
+    {
+      rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 10);
+    }
 
-    Quaternion deltaRotation = Quaternion.Euler(rotationVector * rotationSpeed * Time.deltaTime);
-    rb.MoveRotation(rb.rotation * deltaRotation);
-
+    if (rb.velocity.x <= -10)
+    {
+      rb.velocity = new Vector3(-10, rb.velocity.y, rb.velocity.z);
+    }
+    if (rb.velocity.z <= -10)
+    {
+      rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -10);
+    }
     rb.AddForce(strafeMovement * speed, 0, forwardMovement * speed, ForceMode.Impulse);
   }
 }
