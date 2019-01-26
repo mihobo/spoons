b422bc12e6b35b2fbadcd958ba25312714f962a4
Simon Ashbery <simonashbery@simons-mbp.guest.visitor>
2019-01-26 12:46:02 +0000

    Adds X and Z controls to Player

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  private Rigidbody rb;
+  public float speed = 5;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    float forwardMovement = Input.GetAxis("Vertical");
+    float strafeMovement = Input.GetAxis("Horizontal");
+    Debug.Log(forwardMovement);
+    rb.AddForce(strafeMovement * speed, 0, forwardMovement * speed, ForceMode.Impulse);
+  }
+}
