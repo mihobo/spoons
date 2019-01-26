574e968a112c27faac9757885878cc747db57259
Toby Hamand <tobyhamand@gmail.com>
2019-01-26 14:43:33 +0000

    remove library, add rotation with vectors

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
   private Rigidbody rb;
   public float speed = 5;
+  public float rotationSpeed = 5;
   // Start is called before the first frame update
   void Start()
   {
@@ -22,7 +23,16 @@ public class PlayerController : MonoBehaviour
   {
     float forwardMovement = Input.GetAxis("Vertical");
     float strafeMovement = Input.GetAxis("Horizontal");
-    Debug.Log(forwardMovement);
+    float verticalRotation = Input.GetAxis("LookVertical");
+    float horizontalRotation = Input.GetAxis("LookHorizontal");
+    Vector3 rotationVector = new Vector3(verticalRotation, horizontalRotation, 0);
+
+    Debug.Log(verticalRotation);
+    Debug.Log(horizontalRotation);
+
+    Quaternion deltaRotation = Quaternion.Euler(rotationVector * rotationSpeed * Time.deltaTime);
+    rb.MoveRotation(rb.rotation * deltaRotation);
+
     rb.AddForce(strafeMovement * speed, 0, forwardMovement * speed, ForceMode.Impulse);
   }
 }
